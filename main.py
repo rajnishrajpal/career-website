16200ec69833a3aff156fc9b5b6956ab807adb18
Rajnish Rajpal <rajnishrajpal@gmail.com>
2023-02-19 15:42:01 +0000

    html pages added

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+JOBS=[
+  {
+    "jobid":1,
+    "designation": "backend engineer",
+    "salary": "Rs 12,00,000",
+    "location": "Bangalore"
+  },
+    {
+    "jobid":2,
+    "designation": "frontend engineer",
+    "salary": "Rs 10,00,000",
+    "location": "Delhi"
+  },
+  {
+    "jobid":3,
+    "designation": "Data Engineer",
+    "salary": "Rs 15,00,000",
+    "location": "Remote"
+  },
+  {
+    "jobid":4,
+    "designation": "Data Scientist",
+    "salary": "Rs 20,00,000",
+    "location": "San Francisco"
+  }
+]
+
 @app.route("/")
-def Hello_world():
-  return "Hello World"
+def Hello_apex():
+  return render_template("home.html", jobs=JOBS)
 
 if __name__ == "__main__":
   app.run(host = "0.0.0.0", debug=True)
